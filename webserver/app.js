2ec4a97ee60c45a419b7ec685fdee0c8c047f593
deep005 <guhadeepshanker@gmail.com>
2017-10-22 19:59:08 +0530

    adding subscriber logic

--- webserver/app.js
+++ webserver/app.js
@@ -5,6 +5,8 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 
+var subsocket = require('./lib/subscribe');
+
 server.listen(3000, function(){
     console.log('Server is running on port 3000');
 });
@@ -14,4 +16,17 @@ app.use(express.static('public'));
 
 app.get('/', function(req, res){
    res.sendfile('./public/index.html');
+});
+
+io.socket.on('connection', function(socket){
+    model.get(function(err, data){
+        if(err) return;
+        data.forEach(function(badge){
+           socket.emit('badge', badge);
+        });
+    });
+});
+
+subsocket.on('message', function(message){
+   io.socket.emit('badge', message);
 });
